d2dd369c15bcd3634094dd7e45c31f372b3c7362
sasakirione <sasakirione@gmail.com>
2025-04-08 18:56:55 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+}
+
+group = "com.sasakirione"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(20)
+}
